681e8d1b13988ddb31a436bf0f37d9599dbde7f4
bachar157 <bachar.alhamada76@gmail.com>
2024-01-18 13:17:38 -0600

    Add project files.

---
+++ parking-exercice/Dictionaries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parking_exercice
+{
+    internal class Dictionaries
+    {
+        Dictionary<int, string> CarPark = new Dictionary<int, string>();
+        int CarParkavlable = 4;
+
+        public Dictionary<int, string> readingTheInput()
+        {
+            for (int i = 1; i <= CarParkavlable; i++)
+            {
+                Console.WriteLine("enter your license number to resrve a park for your car:");
+                string licenseNumber = Console.ReadLine();
+                CarPark[i] = licenseNumber;
+            }
+            return CarPark;
+        }
+        public void validatThelicenseNumber() 
+        { 
+            if(readingTheInput() != null) { }
+
+        }
+
+    }
+}
