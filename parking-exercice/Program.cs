c97fb180278b954f8d6eebbc74f9af7c76733a04
Bachar Alhamada <71847632+bachar157@users.noreply.github.com>
2024-01-19 23:04:40 -0600

    Update Program.cs

--- parking-exercice/Program.cs
+++ parking-exercice/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace parking_exercice
+namespace parking_for_cars
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Dictionaries dictionaries = new Dictionaries();
+            dictionaries.createparking();
+            Dictionaries dictionaries1 = new Dictionaries();
+           // dictionaries1.bookingTheParking();
+            Console.ReadLine();
         }
     }
 }
